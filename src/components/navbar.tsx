3e0b16c23c461d594899bc6ff4fec20c9e0fe66d
Kacper Budny <kacperbudny98@gmail.com>
2025-11-21 22:11:57 +0100

    Add logo and favicon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,14 +5,21 @@ import {
   NavbarItem,
 } from "@heroui/navbar";
 import { UserButton } from "@stackframe/stack";
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/assets/logo.png";
 
 export function Navbar() {
   return (
     <HeroUINavbar isBordered>
-      <NavbarBrand>
-        <Link href="/">
-          <h1 className="font-bold text-inherit">BIOLLECTOR</h1>
+      <NavbarBrand className="h-full">
+        <Link href="/" className="flex h-full items-center">
+          <Image
+            src={logo}
+            alt="Biollector logo"
+            className="h-full w-auto p-3"
+            quality={100}
+          />
         </Link>
       </NavbarBrand>
       <NavbarContent justify="end">
