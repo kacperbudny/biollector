7fe0f13dbda746a92ebfa100e8d545b602ec09c8
Kacper Budny <kacperbudny98@gmail.com>
2026-03-08 16:28:22 +0100

    Improvements for HeroUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import { heroui } from "@heroui/theme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(button|card|navbar|spacer|ripple|spinner).js",
+    "./node_modules/@heroui/theme/dist/components/(button|card|navbar|spacer|toast|popover|ripple|spinner).js",
   ],
   theme: {
     extend: {},
